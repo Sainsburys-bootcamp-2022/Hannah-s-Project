c1376e28cd1efbbb26015cbbecd2b5edc1913d46
Hannah Corke <hannah.corke@sainsburys.co.uk>
2022-06-09 16:50:43 +0100

    Comments removed & formatting fixed

--- NoughtsandCrosses/final/academy.js
+++ NoughtsandCrosses/final/academy.js
@@ -1,8 +1,6 @@
-// Make your changes to store and update game state in this file
-
-let board = [[null, null, null], 
-             [null, null, null], 
-             [null, null, null]]
+let board = [[null, null, null],
+[null, null, null],
+[null, null, null]]
 function getNewBoard() {
   return [[null, null, null], [null, null, null], [null, null, null]]
 }
@@ -11,259 +9,156 @@ let noughtsTurn = true
 console.log("Noughts Turn")
 let gameOver = false
 
-// Take the row and column number between 0 and 2
-// (inclusive) and update the game state.
 function takeTurn(row, column) {
-    console.log(board)  //no longer shows null as I removed [row][column]
-    const currentSquare = board[row][column]
+  console.log(board)
+  const currentSquare = board[row][column]
 
-    if (board[row][column] === null && noughtsTurn === true) {
-        //console.log("Noughts Turn")
-        board[row][column] = "nought"
-        noughtsTurn = false
-      //  console.log(board)
-        console.log("Crosses Turn")
-    } else if (board[row][column] === null && noughtsTurn === false) {
-        board[row][column] = "cross"
-        noughtsTurn = true
-    } else
-      {console.log("space already chosen")}
-       
-      console.log("takeTurn was called with row: " + row + ", column:" + column);
-      console.log(`takeTurn was called with row: ${row}, column: ${column}`);
+  if (board[row][column] === null && noughtsTurn === true) {
+    //console.log("Noughts Turn")
+    board[row][column] = "nought"
+    noughtsTurn = false
+    //  console.log(board)
+    console.log("Crosses Turn")
+  } else if (board[row][column] === null && noughtsTurn === false) {
+    board[row][column] = "cross"
+    noughtsTurn = true
+  } else { console.log("space already chosen") }
 
-  }
+  console.log("takeTurn was called with row: " + row + ", column:" + column)
+}
 
-     // console.log(board[row][column]) 
-    
-
-//     let icon = 'X'
-
-// const toggle = () => {
-//     if ( icon === 'X' ) {
-//         icon = 'O';
-//         inst.innerHTML = "<p>Nought's turn<p/>";
-//     }    
-//     else {
-//         icon = 'X';
-//         inst.innerHTML = "<p>Cross's turn<p/>";
-//     }    
-// }
-
-// Return either "noughts", "crosses" or "nobody" if the game is over.
-// Otherwise return null to continue playing.
 function checkWinner() {
-    console.log("checkWinner was called");
-    //return null;
-  //  console.log(board[row][column])
-
-  // const noughtsPlayer = document.getElementById("noughts-player").innerText
-  // const crossesPlayer = document.getElementById("crosses-player").innerText
-
-  //change this because need nobody as third option
-  //draw (no winner) if 9 counters placed, use a counter - each time a play goes add 1 to the counter and when hits 9 becomes nobody
-  //or could I do if no square = null && !Noughts wins && !Crosses wins
+  console.log("checkWinner was called")
 
   //Horizontal wins
 
-     var checkWinner = (board[0][0] === "nought" && board[0][1] === "nought" && board[0][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  var checkWinner = (board[0][0] === "nought" && board[0][1] === "nought" && board[0][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
-   
-   var checkWinner = (board[1][0] === "nought" && board[1][1] === "nought" && board[1][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
- //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[2][0] === "nought" && board[2][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
- //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
-
-    var checkWinner = (board[0][0] === "cross" && board[0][1] === "cross" && board[0][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
- //   console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
-
-    var checkWinner = (board[1][0] === "cross" && board[1][1] === "cross" && board[1][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  var checkWinner = (board[1][0] === "nought" && board[1][1] === "nought" && board[1][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[2][0] === "cross" && board[2][1] === "cross" && board[2][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
- //   console.log(checkWinner)  
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" } 
-
-//Vertical wins
-
-    var checkWinner = (board[0][0] === "nought" && board[1][0] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  var checkWinner = (board[2][0] === "nought" && board[2][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
-   
-   var checkWinner = (board[0][1] === "nought" && board[1][1] === "nought" && board[2][1] === "nought") ? "Noughts wins" : "Noughts doesn't win";
- //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[0][2] === "nought" && board[1][2] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
- //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
+  var checkWinner = (board[0][0] === "cross" && board[0][1] === "cross" && board[0][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  //   console.log(checkWinner)
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
 
-    var checkWinner = (board[0][0] === "cross" && board[1][0] === "cross" && board[2][0] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  var checkWinner = (board[1][0] === "cross" && board[1][1] === "cross" && board[1][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
 
-    var checkWinner = (board[0][1] === "cross" && board[1][1] === "cross" && board[2][1] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  var checkWinner = (board[2][0] === "cross" && board[2][1] === "cross" && board[2][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  //   console.log(checkWinner)  
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
+
+  //Vertical wins
+
+  var checkWinner = (board[0][0] === "nought" && board[1][0] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[0][2] === "cross" && board[1][2] === "cross" && board[2][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
- //   console.log(checkWinner)  
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" } 
-
-//Diagonal wins
-
-    var checkWinner = (board[0][0] === "nought" && board[1][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  var checkWinner = (board[0][1] === "nought" && board[1][1] === "nought" && board[2][1] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[0][2] === "nought" && board[1][1] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  var checkWinner = (board[0][2] === "nought" && board[1][2] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Noughts wins") {
-        return "Noughts" }
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
 
-    var checkWinner = (board[0][0] === "nought" && board[1][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  var checkWinner = (board[0][0] === "cross" && board[1][0] === "cross" && board[2][0] === "cross") ? "Crosses wins" : "Crosses doesn't win";
   //  console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
 
-    var checkWinner = (board[0][2] === "nought" && board[1][1] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
-   // console.log(checkWinner)
-        if (checkWinner === "Crosses wins") {
-        return "Crosses" }
+  var checkWinner = (board[0][1] === "cross" && board[1][1] === "cross" && board[2][1] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  //  console.log(checkWinner)
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
 
-  //need if statements to stop it when one wins if I don't want the if statement after each one
+  var checkWinner = (board[0][2] === "cross" && board[1][2] === "cross" && board[2][2] === "cross") ? "Crosses wins" : "Crosses doesn't win";
+  //   console.log(checkWinner)  
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
+
+  //Diagonal wins
+
+  var checkWinner = (board[0][0] === "nought" && board[1][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  //  console.log(checkWinner)
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
+
+  var checkWinner = (board[0][2] === "nought" && board[1][1] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  //  console.log(checkWinner)
+  if (checkWinner === "Noughts wins") {
+    return "Noughts"
+  }
+
+  var checkWinner = (board[0][0] === "nought" && board[1][1] === "nought" && board[2][2] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  //  console.log(checkWinner)
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
+
+  var checkWinner = (board[0][2] === "nought" && board[1][1] === "nought" && board[2][0] === "nought") ? "Noughts wins" : "Noughts doesn't win";
+  // console.log(checkWinner)
+  if (checkWinner === "Crosses wins") {
+    return "Crosses"
+  }
 
   if (board[0][0] !== null & board[0][1] !== null & board[0][2] !== null & board[1][0] !== null & board[1][1] !== null & board[1][2] !== null & board[2][0] !== null & board[2][1] !== null & board[2][2] !== null) {
     return "Nobody"
+  }
 }
-}
-//   if (board[0][0] !== null && board[0][1] !== null && board[0][2] !== null && board[1][0] !== null && board[1][1] !== null && board[1][2] !== null && board[2][0] !== null && board[2][1] !== null && board[2][2] !== null && checkWinner !== "Noughts wins" && checkWinner !== "Crosses wins") {
-//      return "nobody"
-//  }
-// This works but lets the winner change except on the very first go only when you refresh using the reset button. When refreshing the page it works every time without changing the winner. When I hit Reset it makes it crosses turn first, this could be why. I had console.log("Noughts turn") outside of the takeTurn function. When I put let noughtsTurn = true at the end of the resetGame function it kept Noughts turn as first when I hit the Reset button and this fixed it
 
-
-// if (board[0][0] !== null & board[0][1] !== null & board[0][2] !== null & board[1][0] !== null & board[1][1] !== null & board[1][2] !== null & board[2][0] !== null & board[2][1] !== null & board[2][2] !== null) {
-//     return "nobody"
-// }
-// This works but lets the winner change except on the very first go only when you refresh using the reset button. When refreshing the page it works every time without changing the winner....same as above
-
-//   if (board !== [[null, null, null], [null, null, null], [null, null, null]] && checkWinner !== "Noughts wins" && checkWinner !== "Crosses wins") {
- //  return "nobody"        
-  // }
-  // This shows nobody the whole time until noughts or crosses wins. The winner doesn't change though
-
-
-//   if (checkWinner !== "Noughts wins" && checkWinner !== "Crosses wins") { 
- //  return "nobody"   
-//   }
-//  This shows nobody the whole time until noughts or crosses wins. This also lets the winner change whereas without this the winner doesn't change
-
-
-
-
-
-
- //  row1 = board[0]
-  // console.log(row1)
-
-//   var checkWinner = (board[0] === ["nought", "nought", "nought"]) ? "Noughts wins" : "Noughts doesn't win";
-// console.log(checkWinner)
-
-//if (board[0] === ["nought", "nought", "nought"]){
-//console.log ("Noughts Wins")}
-
-
-
-
-//function checkNoughts() { 
-//    console.log(board[1])}
-
-// I want to check if there are 3 in a row of o or x
-//for loop needed here
-
-//if board = [[x, x, x], [null, null, null], [null, null, null]]
-//if  board[0][0] && board[0][1] && board [0][2] === x 
-//if (condition) {
-
- //if (board !== [[null, null, null], [null, null, null], [null, null, null]]) {console.log ""}
-
- //board[row][column] === null
-
-//for (let i = 0; i < 3; i++) {
-    //if (board[i] === ["nought", "nought", "nought"])
-    //console.log("Noughts is the winner!")}
-
-   
-
-//    if (board[i] === [nought, nought, nought] i<2) 
-//{
- //if board[0] === [nought, nought, nought]
- //if board[1] === [nought, nought, nought]
- //if board[2] === [nought, nought, nought]
- //console.log("Noughts is the winner!")
-//}
-
- //for (let row = 0; row < 3; row++) {
-     //const element = array[row];
- //}
-
- //can set nought/cross to a score and then when checking for 3 in a row, check if the 'points' = 3. Set back to 0 for each new row/column being checked.
-
-// Set the game state back to its original state to play another game.
 function resetGame() {
-    console.log("resetGame was called")
-    board = [[null, null, null], 
-             [null, null, null], 
-             [null, null, null]];
-    noughtsTurn = true
-    gameOver = false
+  console.log("resetGame was called")
+  board = [[null, null, null],
+  [null, null, null],
+  [null, null, null]]
+  noughtsTurn = true
+  gameOver = false
 }
 
-// Return the current board state with either a "nought" or a "cross" in
-// each position. Put a null in a position that hasn't been played yet.
-function getBoard(){ 
-    // if (!gameOver) {
-    console.log("getBoard was called")
-    return board 
-   // } 
+function getBoard() {
+  console.log("getBoard was called")
+  return board
 }
-
-//return [[null, null, null], [null, null, null], [null, null, null]];
-   // if row 0 and column 0, put nought/cross in 0,0 
-//}
-//}
 
 if (typeof exports === 'object') {
-    console.log("Running in Node")
-    // Node. Does not work with strict CommonJS, but only CommonJS-like
-    // environments that support module.exports, like Node.
-    module.exports = {
-        takeTurn,
-        checkWinner,
-        resetGame,
-        getBoard,
-    }
+  console.log("Running in Node")
+  module.exports = {
+    takeTurn,
+    checkWinner,
+    resetGame,
+    getBoard,
+  }
 } else {
-    console.log("Running in Browser")
+  console.log("Running in Browser")
 }
-
--- NoughtsandCrosses/final/connectors.js
+++ NoughtsandCrosses/final/connectors.js
@@ -1,34 +1,19 @@
-// This file contains helper code beyond the first week "Intro to JavaScript" course content.
-// You should not have to make any changes in this file to get your game working.
 
-// Validate academite functions are available
-const functions = ["takeTurn", "getBoard", "checkWinner", "resetGame"];
+const functions = ["takeTurn", "getBoard", "checkWinner", "resetGame"]
 for (f of functions) {
-    const functionObject = window[f];
+    const functionObject = window[f]
     if (typeof functionObject !== "function") {
-        throw `Looks like expected function '${f}' is missing. Double check the function signatures from academy.js are still present and unaltered.`;
+        throw `Looks like expected function '${f}' is missing. Double check the function signatures from academy.js are still present and unaltered.`
     }
 }
 
-// Clear down the elements drawn on the board.
 function clearBoard() {
     for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
         for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
             document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerHTML = ""
         }
-    }  
+    }
 }
-
-// function namesSubmitted() {
-//     const noughtsPlayerName = document.getElementById("noughts-player").value
-//     console.log(noughtsPlayerName)
-//     const crossesPlayerName = document.getElementById("crosses-player").value 
-//     console.log(crossesPlayerName) 
-// }
-
-// const submitButton = document.getElementById("submit")
-// submitButton.addEventListener("click", namesSubmitted)
-
 
 const submitButton = document.getElementById("submit")
 
@@ -39,8 +24,6 @@ function disableTextInput() {
 
 submitButton.addEventListener("click", disableTextInput)
 
-
-// Populate the grid with images based on the board state.
 function drawBoard(board) {
     clearBoard();
     for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
@@ -48,53 +31,45 @@ function drawBoard(board) {
             if (!board[rowIndex][columnIndex]) {
                 continue;
             }
-        const cellText = board[rowIndex][columnIndex] === "nought" ? "⊘" : "✗";
-        document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = cellText;
+            const cellText = board[rowIndex][columnIndex] === "nought" ? "⊘" : "✗";
+            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = cellText;
         }
     }
 }
 // 🔘✖️❍✘✗❌⭕⚪🟣◌∅〇⊚❂⊛⊘✗
 //https://www.i2symbol.com/symbols/circle
 
-
 function isValidRowOrColumn(array) {
-    return Array.isArray(array) && array.length === 3;
+    return Array.isArray(array) && array.length === 3
 }
 
 function isValidColumn(columnArray) {
-    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["nought", "cross", null].includes(item); });
+    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["nought", "cross", null].includes(item) })
 }
 
-// A grid position was clicked call the game's turn function, redraw and then check for a winner.
 function positionClick(rowIndex, columnIndex, event) {
-    takeTurn(rowIndex, columnIndex);
-    const board = getBoard();
+    takeTurn(rowIndex, columnIndex)
+    const board = getBoard()
     if (!isValidRowOrColumn(board) || !board.every(isValidColumn)) {
-        throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board);
+        throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board)
     }
-    drawBoard(board);
-    const winner = checkWinner();
+
+    drawBoard(board)
+    const winner = checkWinner()
 
     if (winner) {
         if (typeof winner !== "string" || !["Noughts", "Crosses", "Nobody"].includes(winner)) {
-            throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner;
+            throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner
         }
-        const winnerName = document.getElementById("winner-name");
-        winnerName.innerText = winner;
-
-
-
-        // const noughtsPlayer = document.getElementById("noughts-player").value  //noughtsPlayer = whatever was typed into noughts player name box an
-        // const noughtsWinnerPlayerName = winner === "Noughts" ? noughtsPlayer : "no"
-        // document.getElementById("winner-name").innerText = noughtsWinnerPlayerName
-        // console.log(noughtsWinnerPlayerName)
+        const winnerName = document.getElementById("winner-name")
+        winnerName.innerText = winner
 
         const noughtsPlayer = document.getElementById("noughts-player").value
-        const noughtsWinnerName = document.getElementById("noughts-player").value === "" ? "Noughts": noughtsPlayer
+        const noughtsWinnerName = document.getElementById("noughts-player").value === "" ? "Noughts" : noughtsPlayer
 
         const crossesPlayer = document.getElementById("crosses-player").value
         const crossesWinnerName = document.getElementById("crosses-player").value === "" ? "Crosses" : crossesPlayer
-        
+
         if (winner === "Noughts") {
             document.getElementById("winner-name").innerText = noughtsWinnerName
             gameOver = true
@@ -110,48 +85,36 @@ function positionClick(rowIndex, columnIndex, event) {
 
         const winnerDisplay = document.getElementById("winner-display");
         winnerDisplay.style.display = "block";
-
-
-//try an if statement where if there is text in the player name box, announce the winner by name, otherwise keeps noughts/crosses
-
-        // const cellText = board[rowIndex][columnIndex] === "nought" ? "⊘" : "✗";
-        // document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = cellText;
-       // document.getElementById("promotioncode").value == ""
     }
 }
 
-// The reset button was clicked, call the game's reset function then reset the DOM.
 function resetClick(event) {
-    resetGame();
-    const winnerName = document.getElementById("winner-name");
-    winnerName.innerText = "";
-    const winnerDisplay = document.getElementById("winner-display");
-    winnerDisplay.style.display = "None";
-    clearBoard();
-    const noughtsName = document.getElementById("noughts-player");
-    noughtsName.value = "";
-    const crossesName = document.getElementById("crosses-player");
-    crossesName.value = "";
+    resetGame()
+    const winnerName = document.getElementById("winner-name")
+    winnerName.innerText = ""
+    const winnerDisplay = document.getElementById("winner-display")
+    winnerDisplay.style.display = "None"
+    clearBoard()
+    const noughtsName = document.getElementById("noughts-player")
+    noughtsName.value = ""
+    const crossesName = document.getElementById("crosses-player")
+    crossesName.value = ""
     document.getElementById("noughts-player").disabled = false
     document.getElementById("crosses-player").disabled = false
 }
 
-// Bind the click events for the grid.
 for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
     for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
-        const gridPosition = document.getElementById(`row-${rowIndex}-column-${columnIndex}`);
-        gridPosition.addEventListener("click", positionClick.bind(null, rowIndex, columnIndex));
+        const gridPosition = document.getElementById(`row-${rowIndex}-column-${columnIndex}`)
+        gridPosition.addEventListener("click", positionClick.bind(null, rowIndex, columnIndex))
     }
 }
 
-// Bind the click event for the reset button.
-const resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", resetClick);
+const resetButton = document.getElementById("reset-button")
+resetButton.addEventListener("click", resetClick)
 
 if (typeof exports === 'object') {
     console.log("Running in Node")
-    // Node. Does not work with strict CommonJS, but only CommonJS-like 
-    // environments that support module.exports, like Node.
     module.exports = {
         clearBoard,
         drawBoard,
@@ -162,4 +125,4 @@ if (typeof exports === 'object') {
     }
 } else {
     console.log("Running in Browser")
-    }
+}
